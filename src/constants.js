f6ab8b463bd4463da2ba6d531478719f5964be29
riyaawadhiya <riaawadhiya8200@gmail.com>
2026-01-15 19:04:56 +0530

    modiied

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const DB_NAME = "YTBACKEND"
+export const DB_NAME = "mernApp";
--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-// # Server entry point
+// import express from "express";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+// import dotenv from "dotenv";
 
+// dotenv.config({ path: "../.env" });
 
+// const app = express();
+
+// // IIFE = Immediately Invoked Function Expression
+// (async () => {
+//   try {
+// // await mongoose.connect("mongodb://localhost:27017/mernApp");
+// await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+//     app.on("error", (error) => {
+//       console.log("Server Error:", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT || 5000, () => {
+//       console.log(`Server running on port ${process.env.PORT || 5000}`);
+//     });
+
+//     console.log("MongoDB Connected Successfully");
+
+//   } catch (error) {
+//     console.error("MongoDB Connection Failed:", error);
+//     process.exit(1);
+//   }
+// })();
